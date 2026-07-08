57201bf60719444503433a5a883f97085aead04d
ramonmsilvabr <ramon.meireles32@gmail.com>
2026-07-08 12:21:10 -0300

    sign-modules.sh: Refatoração de código

--- secureboot/sign-modules.sh
+++ secureboot/sign-modules.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
-# Pega versão do Linux Kernel
+
+# Pega versão do pacote de kernel do Fedora
 KERNEL_VERSION="$(rpm -q kernel-core --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 # Configurações - Altere os caminhos para onde estão seus arquivos
 PRIV_KEY="akmods.priv"
@@ -10,57 +11,41 @@ chmod 400 $PRIV_KEY
 
 echo "Instalando kernel-devel"
 dnf install -y kernel-devel 
-# 1. Localizar o utilitário sign-file no Silverblue/Fedora
-# O link 'build' em /lib/modules sempre aponta para os headers do kernel atual
+
+# kernel-devel provem o código fonte e o utilitário de assinatura
 SIGN_FILE="/usr/src/kernels/${KERNEL_VERSION}/scripts/sign-file"
 
+# Filtra a existência do utilitário de assinatura
 if [ ! -f "$SIGN_FILE" ]; then
-    echo "Erro: Utilitário sign-file não encontrado."
-    echo "Certifique-se de que o pacote 'kernel-devel' está instalado (dnf -y install kernel-devel)."
     exit 1
 fi
 
+# Diretório dos módulos de kernel
 TARGET_DIR="/usr/lib/modules/${KERNEL_VERSION}/extra/"
 
-# 2. Busca e Processamento
-# O find busca arquivos que terminam em .ko ou variações compactadas
+# Checa a existência de módulos compactados no TARGET_DIR
 find "$TARGET_DIR" -type f \( -name "*.ko" -o -name "*.ko.xz" -o -name "*.ko.zst" \) | while read -r MODULE_PATH; do
-
     EXTENSION="${MODULE_PATH##*.}"
     RECOMPRESS=""
     CURRENT_FILE="$MODULE_PATH"
-
-    # 3. Tratamento de compactação
     if [ "$EXTENSION" == "xz" ]; then
-        echo "Descompactando: $MODULE_PATH"
-        sudo xz -d "$MODULE_PATH"
+        xz -d "$MODULE_PATH"
         CURRENT_FILE="${MODULE_PATH%.xz}"
         RECOMPRESS="xz"
     elif [ "$EXTENSION" == "zst" ]; then
-        echo "Descompactando: $MODULE_PATH"
-        sudo unzstd --rm "$MODULE_PATH"
+        unzstd --rm "$MODULE_PATH"
         CURRENT_FILE="${MODULE_PATH%.zst}"
         RECOMPRESS="zstd"
     fi
-
-    # 4. Assinatura
-    echo "Assinando: $CURRENT_FILE"
-    sudo "$SIGN_FILE" sha256 "$PRIV_KEY" "$DER_CERT" "$CURRENT_FILE"
-
-    # 5. Recompactação
+    "$SIGN_FILE" sha256 "$PRIV_KEY" "$DER_CERT" "$CURRENT_FILE"
     if [ "$RECOMPRESS" == "xz" ]; then
-        echo "Recompactando em XZ..."
-        sudo xz -f -C crc32 "$CURRENT_FILE"
+        xz -f -C crc32 "$CURRENT_FILE"
     elif [ "$RECOMPRESS" == "zstd" ]; then
-        echo "Recompactando em ZST..."
-        sudo zstd --rm -19 -f "$CURRENT_FILE"
+        zstd --rm -19 -f "$CURRENT_FILE"
     fi
 done
 
-echo "Limpeza de chaves de criptografia por segurança"
 rm -rfv "$PRIV_KEY" "$DER_CERT"
 
-echo "Removendo Kernel Devel"
+# Limpa kernel-devel
 dnf remove -y kernel-devel 
-
-echo "Processo concluído! Todos os módulos foram assinados e os arquivos temporários foram removidos."
